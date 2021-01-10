333708c2f204f102fa6cfed54c1cfd8b3eb8785b
diek <d_kearney@bellaliant.net>
2021-01-09 21:02:55 -0300

    Functional test checks input

--- superlist/config/urls.py
+++ superlist/config/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from lists import views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_page, name='home')
+    path('', include('lists.urls'))
 ]
